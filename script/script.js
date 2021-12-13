2662f4ae0ed9c2c110628a294c415215a091546c
David Vergaray <david.vergaray.a@gmail.com>
2021-12-13 19:46:08 -0300

    Fix linters

--- script/script.js
+++ script/script.js
@@ -1,13 +1,13 @@
-let popUp = document.querySelector(".pop-menu");
-let button = document.getElementById("drop-image");
-let innerButtons = document.querySelectorAll(".close");
+const popUp = document.querySelector('.pop-menu');
+const button = document.getElementById('drop-image');
+const innerButtons = document.querySelectorAll('.close');
 
-button.onclick = function() {
-  popUp.classList.toggle("pop-menu-toggle");
+button.onclick = function () {
+  popUp.classList.toggle('pop-menu-toggle');
 }
 
 innerButtons.forEach(item => {
-  item.addEventListener("click", event => {
-    popUp.classList.toggle("pop-menu-toggle");
-  })
-})
+  item.addEventListener('click', event => {
+    popUp.classList.toggle('pop-menu-toggle');
+  });
+});
